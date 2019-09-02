b01a266cd9a44a0b87a4def5d77523c22228a193
Anupa Rajasinghe <49247867+anuparajasinghe@users.noreply.github.com>
2019-09-02 20:52:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,29 +18,7 @@ bot.remove_command("help")
 async def on_message(message):
         if message.author.bot:
             return
-
-        elif message.content.startswith ("<8ball"):
-            await bot.send_message(message.channel, random.choice([":8ball: | **{},** It is Certain ".format(message.author.name),
-                                                                  ":8ball: | **{},** It is Decidedly, So ?".format(message.author.name),
-                                                                  ":8ball: | **{},** Without a doubt ".format(message.author.name),
-                                                                  ":8ball: | **{},** Yes, definitely  ".format(message.author.name),
-                                                                  ":8ball: | **{},** You may rely on it ".format(message.author.name),
-                                                                  ":8ball: | **{},** As I see it, yes  ".format(message.author.name),
-                                                                  ":8ball: | **{},** Most likely  ".format(message.author.name),
-                                                                  ":8ball: | **{},** Outlook Looks Good  ".format(message.author.name),
-                                                                  ":8ball: | **{},** Yes Bro ".format(message.author.name),
-                                                                  ":8ball: | **{},** Signs point to yes ".format(message.author.name),
-                                                                  ":8ball: | **{},** Reply lazy try again ".format(message.author.name),
-                                                                  ":8ball: | **{},** Ask again later ".format(message.author.name),
-                                                                  ":8ball: | **{},** Better not tell you now ".format(message.author.name),
-                                                                  ":8ball: | **{},** Cannot predict now ".format(message.author.name),
-                                                                  ":8ball: | **{},** Concentrate and ask again ".format(message.author.name),
-                                                                  ":8ball: | **{},** Don't count on it  ".format(message.author.name),
-                                                                  ":8ball: | **{},** My reply is no ".format(message.author.name),
-                                                                  ":8ball: | **{},** My sources say no ".format(message.author.name),
-                                                                  ":8ball: | **{},** Outlook is Not So Good  ".format(message.author.name),
-                                                                  ":8ball: | **{},** Very doubtful  ".format(message.author.name)]))
-
+ 
         elif message.content.startswith("<tell"):
             if message.author.id == "429118689367949322":
                 text = message.content[len('<tell'):].strip()
@@ -161,32 +139,5 @@ async def troll(ctx, user: discord.Member = None):
     else:
         await bot.send_message(ctx.message.channel,"**{} WAS BANNED** <:verified:560383768372838400>  ".format(user))
         await bot.delete_message(ctx.message)
-
-@bot.command(pass_context=True)
-async def lovecal(ctx , user: discord.Member = None):
-    if user is None:
-        await bot.say(":x: Mention a User to Check The Love Percenatge")
-    if user.id == ctx.message.author.id:
-        embed=discord.Embed(color=0xff0000)
-        embed.add_field(name=":heart: Love Calculator",value="Love Percentage between {} and {}\n **0%**".format(ctx.message.author.mention , user.mention), inline=False)
-        await bot.send_message(ctx.message.channel,embed=embed)
-    else:
-        embed=discord.Embed(color=0xff0000)
-        embed.add_field(name=":heart: Love Calculator", value="Love Percentage between {} and {}\n **{}%**".format(ctx.message.author.mention , user.mention , random.randint(1,100)), inline=False)
-        await bot.send_message(ctx.message.channel,embed=embed)
-
-
-@bot.command(pass_context=True)
-async def link(ctx):
-    embed=discord.Embed(title="Subscribe to Ma Boss!!", url="https://www.youtube.com/channel/UC0bZpIWLn_YEjkyTyvAadtQ?view_as=subscriber")
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/557874110944903178/564336911293087744/social-media-and-marketing.png")
-    embed.set_author(name="the AnimatedStick", icon_url="https://images-ext-1.discordapp.net/external/QTCOBVY3LPk9lQtkaKJwJ7KbSqwYakN8IMGkqs0OqdQ/https/cdn.discordapp.com/icons/554641659888140308/c38ea2979140e6b1a8f8ea30473ac7bf.jpg")
-    embed.add_field(name="Discord Server", value="[Click](http://bit.ly/tASDiscord)", inline=False)
-    embed.add_field(name="YouTube Channel", value="[Click](http://bit.ly/tASYouTUBE)", inline=False)
-    embed.add_field(name="Twitter", value="[Click](http://bit.ly/tASTwitter)", inline=False)
-    embed.add_field(name="Instagram", value="Soon!", inline=False)
-    embed.set_footer(text="the AnimatedBot" , icon_url="https://images-ext-1.discordapp.net/external/QTCOBVY3LPk9lQtkaKJwJ7KbSqwYakN8IMGkqs0OqdQ/https/cdn.discordapp.com/icons/554641659888140308/c38ea2979140e6b1a8f8ea30473ac7bf.jpg")
-    await bot.say(embed=embed)
-
 
 bot.run(process.env.BOT_TOKEN)
